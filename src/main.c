ebdde725dfb6bec7ee00524c5fb614e563244b0c
Theo Chartier <tchartie@2D3.42angouleme.fr>
2023-12-04 02:25:34 +0100

    Fix libft Makefile

--- src/main.c
+++ src/main.c
@@ -1,6 +1,10 @@
 #include "push_swap.h"
+#include "libft.h"
 
 int main(int argc, char **argv)
 {
-
+    if (argc == 1 || (argc == 2 && !argv[1][0]))
+        return (1);
+    else if (argc == 2)
+        ft_split(argv[1], ' ');
 }
